a5721f24cad4ccff07446a9be076f89f70ad0f3b
Rastzerr <49648201+Rastzer@users.noreply.github.com>
2022-07-08 02:17:12 +0700

    Create crc16.ino

---
+++ Master/crc16.ino
@@ -0,0 +1,27 @@
+#include "CRC16.h"
+#include "CRC.h"
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(2, 3);
+
+CRC16 crc;
+
+char str[50];
+
+int aktifMenu = 0, pilihCRC = 0;
+String code = "";
+
+String inString = "", massage = "";
+bool comComp = false;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  mySerial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  menu();
+  phars();
+}
