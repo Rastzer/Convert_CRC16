77f05da2f15a8485af095720b31c24dc7d5b014e
Rastzerr <49648201+Rastzer@users.noreply.github.com>
2022-07-23 04:17:21 +0700

    Update menu.ino

--- Master/menu.ino
+++ Master/menu.ino
@@ -1,25 +1,143 @@
 void menu() {
   switch (aktifMenu) {
     case 0:
-      Serial.print("Masukkan Code ASCII :\n");
+      Serial.print("Masukkan data dalam ASCII :\n");
       massage = "";
       comComp = false;
       inString = "";
       aktifMenu = 1;
       break;
+    case 1:
+      if (comComp == true) {
+        massage += inString;
+        massage.toCharArray(str, massage.length());
+        aktifMenu = 2;
+      }
+      break;
     case 2:
-      Serial.println("Pilih polynomial :\n1.ARC\n2.CCITT - FALSE\n3.XMODEM");
-      Serial.println("===========================================");
-      comComp = false;
-      inString = "";
+      Serial.print("Code             : " + massage);
+      Serial.print("HEX              : ");
+      for (int i = 0; i < massage.length() - 1; i++) {
+        Serial.print(massage[i], HEX);
+      }
+      Serial.print("\n");
+      Serial.print("===========================================\n");
       aktifMenu = 3;
       break;
+    case 3:
+      Serial.print("Calculate CRC ");
+      for (int i = 0; i < 10; i++) {
+        Serial.print(".");
+        delay(100);
+      }
+      Serial.print("\n");
+      arc();
+      Serial.print("Getting CRC ");
+      for (int i = 0; i < 10; i++) {
+        Serial.print(".");
+        delay(100);
+      }
+      Serial.print("\n");
+      Serial.print("getCRC           : ");
+      Serial.println(crc.getCRC(), HEX);
+      Serial.print("===========================================\n");
+      aktifMenu = 4;
+      break;
     case 4:
-      Serial.println("Kirim CRC-16?\n1.Yes\n2.No");
-      Serial.println("===========================================");
+      Serial.print("Getting CodeWord ");
+      for (int i = 0; i < 10; i++) {
+        Serial.print(".");
+        delay(100);
+      }
+      Serial.print("\n");
+      Serial.print("CodeWord         : ");
+      for (int i = 0; i < massage.length() - 1; i++) {
+        Serial.print(massage[i]);
+      }
+      Serial.println(crc.getCRC(), HEX);
+      Serial.println("Or");
+      Serial.print("CodeWord         : ");
+      for (int i = 0; i < massage.length() - 1; i++) {
+        Serial.print(massage[i], HEX);
+      }
+      Serial.println(crc.getCRC(), HEX);
+      Serial.print("===========================================\n");
+      aktifMenu = 5;
+      break;
+    case 5:
+      Serial.println("Send data ?");
+      Serial.println("1. Send Data Normal\n2. Send Data Error\n3. No");
+      Serial.print("===========================================\n");
       comComp = false;
       inString = "";
-      aktifMenu = 5;
+      aktifMenu = 6;
+      break;
+    case 6:
+      if (comComp == true) {
+        if (inString.toInt() == 1) {
+          Serial.print("Sending CodeWord ");
+          for (int i = 0; i < 10; i++) {
+            Serial.print(".");
+            delay(100);
+          }
+          Serial.print("\n");
+          mySerial.print("$");
+          for (int i = 0; i < massage.length() - 1; i++) {
+            mySerial.print(massage[i]);
+          }
+          mySerial.print("&");
+          mySerial.print("$");
+          for (int i = 0; i < massage.length() - 1; i++) {
+            mySerial.print(massage[i], HEX);
+          }
+          mySerial.print("&");
+          mySerial.print("$");
+          mySerial.print(crc.getCRC(), HEX);
+          mySerial.print("&");
+          mySerial.print("\n");
+          Serial.print("===========================================\n");
+          crc.reset();
+          aktifMenu = 0;
+        }
+        if (inString.toInt() == 2) {
+          Serial.print("Create Error Data ");
+          for (int i = 0; i < 10; i++) {
+            Serial.print(".");
+            delay(100);
+          }
+          Serial.print("\n");
+          Serial.print("Sending CodeWord ");
+          for (int i = 0; i < 10; i++) {
+            Serial.print(".");
+            delay(100);
+          }
+          Serial.print("\n");
+          //          mySerial.print("$ascii&$");
+          mySerial.print("$");
+          for (int i = 0; i < massage.length() - 1; i++) {
+            mySerial.print(massage[i]);
+          }
+          //          mySerial.print(crc.getCRC(), HEX);
+          mySerial.print("&");
+          //          mySerial.print("$hex&$");
+          mySerial.print("$");
+          for (int i = 0; i < massage.length() - 1; i++) {
+            mySerial.print(massage[i], HEX + 1);
+          }
+          mySerial.print("&");
+          mySerial.print("$");
+          mySerial.print(crc.getCRC(), HEX);
+          mySerial.print("&");
+          mySerial.print("\n");
+          Serial.print("===========================================\n");
+          crc.reset();
+          aktifMenu = 0;
+        }
+        if (inString.toInt() == 3) {
+          crc.reset();
+          aktifMenu = 0;
+        }
+      }
       break;
   }
 }
